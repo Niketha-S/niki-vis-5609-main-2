4f6f88759c174687eac5ad69b0be47c1b1c97032
Niketha-S <84368079+Niketha-S@users.noreply.github.com>
2025-02-28 17:38:32 -0600

    href correction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
 <h1>Niketha's Assignments</h1> 
 
 
-<a href="https://niketha-s.github.io/niki-vis-5609-main-2/" target="_blank" rel="noopener noreferrer">
+<a href="/niki-vis-5609-main-2/A0" target="_blank" rel="noopener noreferrer">
   A0<br>
 </a>
 
-<a href="https://niketha-s.github.io/niki-vis-5609-main-2/A1" target="_blank" rel="noopener noreferrer">
+<a href="/niki-vis-5609-main-2/A1" target="_blank" rel="noopener noreferrer">
   A1
 </a>
 
+
+<a href="/niki-vis-5609-main-2/A2" target="_blank" rel="noopener noreferrer">
+  A2
+</a>
 
 
 
