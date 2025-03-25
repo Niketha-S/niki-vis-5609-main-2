b19292317b9af9eb397fd0359ed24fdcc0390a4e
Niketha-S <84368079+Niketha-S@users.noreply.github.com>
2025-03-25 08:40:45 -0500

    Changed json files to resolve merge conflicts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,11 @@
 
 <a href="{base}/A0">A0</a> <br>
 <a href="{base}/A1">A1</a> <br>
-<<<<<<< HEAD
-<a href="{base}/A2">A2</a>
-=======
 <a href="{base}/A2">A2</a> <br>
-<a href="{base}/A3">A3</a>
->>>>>>> d8f3710 ( A3)
+<a href="{base}/A3">A3</a> <br>
 
+<!-- Alternative approach for external links -->
 <!-- <h1>Niketha's Assignments</h1> 
-
 
 <a href="/niki-vis-5609-main-2/A0" target="_blank" rel="noopener noreferrer">
   A0<br>
@@ -24,16 +20,10 @@
   A1
 </a>
 
-
 <a href="/niki-vis-5609-main-2/A2" target="_blank" rel="noopener noreferrer">
   A2
-<<<<<<< HEAD
-=======
 </a>
+
 <a href="/niki-vis-5609-main-2/A3" target="_blank" rel="noopener noreferrer">
   A3
->>>>>>> d8f3710 ( A3)
 </a> -->
-
-
-
